fbce79cf2f4e405dee94bd9d518e05f50f58dac1
= <play.giver.007@gmail.com>
2020-10-14 17:29:54 -0300

    Marcador do mapa

--- public/scripts/create-orphanage.js
+++ public/scripts/create-orphanage.js
@@ -2,3 +2,28 @@
   Next Level Week #3
   Create Orphanage Script
  */
+
+const map = L.map('mapid').setView([-27.2166991, -49.6471861], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+});
+
+let marker;
+
+map.on('click', (event) => {
+  
+  let lat = event.latlng.lat;
+  let lng = event.latlng.lng;
+
+  // marker && map.removeLayer(marker);
+  if(marker) marker.removeFrom(map);
+
+  marker = L.marker([lat, lng], { icon })
+            .addTo(map);
+
+});
